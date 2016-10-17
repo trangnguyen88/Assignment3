010d8f8e6eddfa2c5e8c6789418e1ed46260b62c
trang <trang_nguyen@csu.fullerton>
2016-10-17 15:19:02 -0700

    first commit

---
+++ client/app.js
@@ -0,0 +1,68 @@
+
+var main=function(){
+
+};
+
+var retrieveAverage = function() {
+    var nums = { numbers : $(".excersice1 input").val().split(",").map(Number) };
+    $.post('http://localhost:4000/average', nums, function(res) {
+	average = res.result;        
+	console.log(res.result);
+        alert(res.result);
+       
+    })
+   
+};
+
+var theLargest = function() {
+    var nums = { numbers : $(".excersice2 input").val().split(",").map(Number) };
+    $.post('http://localhost:4000/largestNumber', nums, function(res) {
+	largestNumber = res.result;        
+	console.log(res.result);
+        alert(res.result);
+       
+    })
+   
+};
+
+var atLeastOneEven=function(){
+	var nums = { numbers : $(".excersice3 input").val().split(",").map(Number) };
+   	 $.post('http://localhost:4000/oneEven', nums, function(res) {
+	oneEven = res.result;        
+	console.log(res.result);
+        alert(res.result);
+       
+    })
+};
+
+var allAreEven=function(){
+	var nums = { numbers : $(".excersice4 input").val().split(",").map(Number) };
+   	 $.post('http://localhost:4000/noOdd', nums, function(res) {
+	noOdd = res.result;        
+	console.log(res.result);
+        alert(res.result);
+       
+    })
+};
+
+var stringFound=function(){
+	var nums = { mainString : $(".ex5").val().split(",") , searchString : $(".ex51 ").val() };
+   	 $.post('http://localhost:4000/isStringFound', nums, function(res) {
+	isStringFound = res.result;     
+	console.log(res.result);
+        alert(res.result);
+       
+    })
+};
+
+var stringFoundNTimes=function(){
+	var nums = { mainString : $(".ex6").val().split(",") , searchString : $(".ex61 ").val(),numbers: $(".ex62").val() };
+   	 $.post('http://localhost:4000/stringFoundNTimes', nums, function(res) {
+	isStringFound = res.result;     
+	console.log(res.result);
+        alert(res.result);
+       
+    })
+};
+
+$(document).ready(main);
---
+++ server.js
@@ -0,0 +1,127 @@
+
+var express = require("express");
+var http = require("http");
+var parser = require("body-parser");
+var app = express();
+app.use(express.static(__dirname + '/client'));
+
+app.use(parser.json());
+app.use(parser.urlencoded({extened: true}));
+http.createServer(app).listen(4000);
+
+app.post("/average",
+  function(req,res){
+     console.log("Request Received :" + req.body.numbers);
+     var result = average(req.body.numbers);
+     var reply = { 'result': result };
+     res.json(reply);	
+     console.log("Result :" + result + " sent back");
+  }
+);
+app.post("/largestNumber",
+  function(req,res){
+     console.log("Request Received :" + req.body.numbers);
+     var result = largestNumber(req.body.numbers);
+     var reply = { 'result': result };
+     res.json(reply);	
+     console.log("Result :" + result + " sent back");
+  }
+);
+app.post("/oneEven",
+  function(req,res){
+     console.log("Request Received :" + req.body.numbers);
+     var result = oneEven(req.body.numbers);
+     var reply = { 'result': result };
+     res.json(reply);	
+     console.log("Result :" + result + " sent back");
+  }
+);
+app.post("/noOdd",
+  function(req,res){
+    console.log("Request Received :" + req.body.numbers);
+     var result = noOdd(req.body.numbers);
+    var reply = { 'result': result };
+     res.json(reply);	
+    console.log("Result :" + result + " sent back");
+  }
+);
+
+app.post("/isStringFound",
+  function(req,res){
+     console.log("Request Received :" + req.body.mainString + " search string : " + req.body.searchString);
+     var result = checkAvailability(req.body.mainString,req.body.searchString);
+     var reply = { 'result': result };
+     res.json(reply);	
+     console.log("Result :" + result + " sent back");
+  }
+);
+app.post("/stringFoundNTimes",
+  function(req,res){
+     console.log("Request Received :" + req.body.mainString + " search string : " + req.body.searchString+" Number of times: "+req.body.numbers);
+     var result = checkStringNTimes(req.body.mainString,req.body.searchString,req.body.numbers);
+     var reply = { 'result': result };
+     res.json(reply);	
+     console.log("Result :" + result + " sent back");
+  }
+);
+
+var average=function(nums){
+        console.log("Calculating Average for numbers " + nums);
+    
+	var sum=0;
+	var averageCal,i;
+	
+	for(i=0;i<nums.length;i++)
+	{
+          sum=sum +parseInt( nums[i]);
+        }
+        
+	averageCal=sum/(nums.length);
+        return averageCal;
+};
+
+var largestNumber=function(nums){
+	var largest;
+	largest=nums[0];
+	var i;
+	for(i=0;i<nums.length;i++)
+	{
+		if(nums[i]>largest)
+		{
+			largest=nums[i];		
+		}
+	}
+	return largest;
+};
+
+var oneEven = function (nums) {
+   return nums.some(atLeastOneEven);
+}
+function atLeastOneEven(value){
+	return (value%2==0);
+}
+var noOdd=function(nums){
+	return nums.every(allEven);
+};
+function allEven(value){
+	return (value%2==0);
+}
+
+
+function checkAvailability(arr, val) {
+    for(i=0;i<arr.length;i++){
+        if(arr[i] == val)
+         return true;
+    }
+    return false;
+}
+
+function checkStringNTimes(arr,val,num){
+	var count=0,
+	    i;
+	for(i=0;i<arr.length;i++){
+		if(arr[i]==val){count++;}
+	}
+	if (count==num){return true;}
+	else{return false;}
+}
