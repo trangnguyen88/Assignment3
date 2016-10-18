980bb347ec3dc14552e1f15e415a75b8c4bd22ce
trang <trang_nguyen@csu.fullerton>
2016-10-18 14:23:44 -0700

    changed the port from 4000 to 3000 in POST method

--- client/app.js
+++ client/app.js
@@ -5,7 +5,7 @@ var main=function(){
 
 var retrieveAverage = function() {
     var nums = { numbers : $(".excersice1 input").val().split(",").map(Number) };
-    $.post('http://localhost:4000/average', nums, function(res) {
+    $.post('http://localhost:3000/average', nums, function(res) {
 	average = res.result;        
 	console.log(res.result);
         alert(res.result);
@@ -16,7 +16,7 @@ var retrieveAverage = function() {
 
 var theLargest = function() {
     var nums = { numbers : $(".excersice2 input").val().split(",").map(Number) };
-    $.post('http://localhost:4000/largestNumber', nums, function(res) {
+    $.post('http://localhost:3000/largestNumber', nums, function(res) {
 	largestNumber = res.result;        
 	console.log(res.result);
         alert(res.result);
@@ -27,7 +27,7 @@ var theLargest = function() {
 
 var atLeastOneEven=function(){
 	var nums = { numbers : $(".excersice3 input").val().split(",").map(Number) };
-   	 $.post('http://localhost:4000/oneEven', nums, function(res) {
+   	 $.post('http://localhost:3000/oneEven', nums, function(res) {
 	oneEven = res.result;        
 	console.log(res.result);
         alert(res.result);
@@ -37,7 +37,7 @@ var atLeastOneEven=function(){
 
 var allAreEven=function(){
 	var nums = { numbers : $(".excersice4 input").val().split(",").map(Number) };
-   	 $.post('http://localhost:4000/noOdd', nums, function(res) {
+   	 $.post('http://localhost:3000/noOdd', nums, function(res) {
 	noOdd = res.result;        
 	console.log(res.result);
         alert(res.result);
@@ -47,7 +47,7 @@ var allAreEven=function(){
 
 var stringFound=function(){
 	var nums = { mainString : $(".ex5").val().split(",") , searchString : $(".ex51 ").val() };
-   	 $.post('http://localhost:4000/isStringFound', nums, function(res) {
+   	 $.post('http://localhost:3000/isStringFound', nums, function(res) {
 	isStringFound = res.result;     
 	console.log(res.result);
         alert(res.result);
@@ -57,7 +57,7 @@ var stringFound=function(){
 
 var stringFoundNTimes=function(){
 	var nums = { mainString : $(".ex6").val().split(",") , searchString : $(".ex61 ").val(),numbers: $(".ex62").val() };
-   	 $.post('http://localhost:4000/stringFoundNTimes', nums, function(res) {
+   	 $.post('http://localhost:3000/stringFoundNTimes', nums, function(res) {
 	isStringFound = res.result;     
 	console.log(res.result);
         alert(res.result);
